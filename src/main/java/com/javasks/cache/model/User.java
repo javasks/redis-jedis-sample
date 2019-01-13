5385f27cbe2b4ba726009804fe4b8daeae9e0d19
sandeep <ankurmall89@gmail.com>
2019-01-13 21:06:03 +0530

    Initial Commit

---
+++ src/main/java/com/javasks/cache/model/User.java
@@ -0,0 +1,45 @@
+package com.javasks.cache.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+public class User implements Serializable {
+
+    private String id;
+    private String name;
+    private Long salary;
+
+    /*public User(String id, String name, Long salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }*/
+
+    /*public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Long salary) {
+        this.salary = salary;
+    }*/
+}
